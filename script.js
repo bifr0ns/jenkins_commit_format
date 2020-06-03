d20314c63b2edde1d886bfb5eb77d6c2afb22a1c
Hector Trejo <hate2411@gmail.com>
2020-06-03 09:04:54 -0500

    Heroku initial commit

--- script.js
+++ script.js
@@ -1,61 +1,64 @@
 function giveFormat(editor) {
-    var text = document.getElementById('instructions').value
+    var text = document.getElementById("instructions").value;
 
-    if(text == null)
-        return 0;
-    if(editor == 'svn') {
-        var textFormated = svnFormat(text)
+    if (text == null) return 0;
+    if (editor == "svn") {
+        var textFormated = svnFormat(text);
 
-        result.style.visibility = "visible"; 
-        btnCopy.style.visibility = "visible"; 
+        result.style.visibility = "visible";
+        btnCopy.style.visibility = "visible";
 
-        result.value = textFormated
+        result.value = textFormated;
     }
 }
 
 function svnFormat(text) {
-    var var1 = text.split("\n")
-    
-    for(i = 0; i < var1.length; i++) {
-        var1[i] = var1[i].replace(",", "@")
-        var1[i] = var1[i].substr(1)
+    var var1 = text.split("\n");
 
-        var firstOcurrence = var1[i].indexOf('/')
-        var1[i] = var1[i].slice(firstOcurrence+1)
+    for (i = 0; i < var1.length; i++) {
+        var1[i] = var1[i].replace(",", "@");
+        var1[i] = var1[i].substr(1);
 
-        var1[i] = var1[i].split("/")
+        var firstOcurrence = var1[i].indexOf("/");
+        var1[i] = var1[i].slice(firstOcurrence + 1);
+
+        var1[i] = var1[i].split("/");
     }
 
-    return returnFormat(var1)
+    return returnFormat(var1);
 }
 
 function returnFormat(array) {
-    var typeOfLine = ["Packages", "PackageBodies", "Tables", "Scripts"]
-    var arrayFormat = []
-    var newFormat = ""
+    var typeOfLine = ["Packages", "PackageBodies", "Tables", "Scripts"];
+    var arrayFormat = [];
+    var newFormat = "";
 
-    typeOfLine.forEach(type => {
-        for(i = 0; i < array.length; i++) {
-            if(array[i][1] == type) {
-                arrayFormat.push(array[i])
+    typeOfLine.forEach((type) => {
+        for (i = 0; i < array.length; i++) {
+            if (array[i][1] == type) {
+                arrayFormat.push(array[i]);
             }
         }
     });
 
-    for(i = 0; i < arrayFormat.length; i++) {
-        newFormat += arrayFormat[i][0] + "/" + arrayFormat[i][1] + "/" + arrayFormat[i][2]
-        if(i < arrayFormat.length - 1)
-            newFormat += "|"
+    for (i = 0; i < arrayFormat.length; i++) {
+        newFormat +=
+            arrayFormat[i][0] +
+            "/" +
+            arrayFormat[i][1] +
+            "/" +
+            arrayFormat[i][2];
+        if (i < arrayFormat.length - 1) newFormat += "|";
     }
-    
-    return newFormat
+
+    return newFormat;
 }
 
 function copy() {
-    var copyText = document.getElementById("result")
-  
+    var copyText = document.getElementById("result");
+
     copyText.select();
-    copyText.setSelectionRange(0, 99999) /*For mobile devices*/
-  
-    document.execCommand("copy")
-  } 
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    document.execCommand("copy");
+}
